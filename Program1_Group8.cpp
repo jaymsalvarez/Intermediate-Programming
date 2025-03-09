15b034ef836db3618ace611b6f904326fda3efec
jaymsalvarez <galupojrgeorge_bsit@plmun.edu.ph>
2025-03-09 21:58:06 +0800

    Add files via upload

---
+++ Program1_Group8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	string Fname, Sname, Lname ;
+	string month;
+	int day;
+	int year;
+	
+	cout << "Enter your name: "; cin >> name;
+	
+	cout << "Enter Your Birthdate"<<endl;
+	
+	cout << "Enter Month: "; cin >> month;
+	cout << "Enter Day: "; cin >> day;
+	cout << "Enter Year: "; cin >> year;
+	
+	
+	return 0;
+}
